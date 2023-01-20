38af0c565e607133cfa036bd5038e13315641902
Rajnish Kumar Singh <rajnish2409gkp@gmail.com>
2023-01-20 12:41:56 +0000

    idk

---
+++ components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="drawer">
+        <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content flex flex-col">
+            <div class="relative">
+                <div class="sticky top-0 left-0 right-0">
+                    <div class="w-full navbar bg-base-300">
+                        <div class="flex-none">
+                            <label for="my-drawer-3" class="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    class="inline-block w-5 h-5 stroke-current">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M4 6h16M4 12h16M4 18h16"></path>
+                                </svg>
+                            </label>
+                        </div>
+                        <div class="flex-1">
+                            <a class="btn btn-ghost normal-case text-xl" href="/">Anipi</a>
+                        </div>
+                        <div class="flex-none">
+                            <button class="btn btn-ghost btn-circle">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                                    stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                                </svg>
+                            </button>
+                            <label for="my-drawer-3" class="btn btn-ghost btn-circle avatar">
+                                <div class="w-10 rounded-full">
+                                    <img src="https://img.icons8.com/fluency/96/null/name.png" />
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <slot></slot>
+        </div>
+
+        <div class="drawer-side">
+            <label for="my-drawer-3" class="drawer-overlay"></label>
+            <ul class="menu p-4 w-80 bg-base-100">
+                <div>
+                    <li>
+                        <a href="/profile">Profile</a>
+                    </li>
+                    <li>
+                        <a>Logout</a>
+                    </li>
+                </div>
+                <li><a>Login</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="py-12 px-10">
+        <article class="prose max-w-none">
+            <h4>Anipi</h4>
+        </article>
+        <div class="divider"></div>
+
+
+    </div>
+</template>
+
+<script setup>
+const user = useSupabaseUser()
+</script>
